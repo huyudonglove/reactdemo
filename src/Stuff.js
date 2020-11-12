bf07a692b93e48cbd29e4932e6ffe64382f47a66
huyudong <379145942@qq.com>
2020-11-12 18:18:18 +0800

    'electron'

--- src/Stuff.js
+++ src/Stuff.js
@@ -1,12 +1,31 @@
-import React, { Component } from "react";
+import React from "react";
 
 class Stuff extends React.Component {
+    constructor(props){
+         super(props);
+         console.log(props);
+         props.history.listen((location)=>{
+             console.log(123)
+              console.log(77);
+
+
+         })
+    }
+    componentDidMount(){
+       //this.goTo();
+    }
+    goTo(){
+        console.log(456)
+        console.log(this.props,789)
+        this.props.history.push({
+            path:'/start'
+        })
+    }
     render() {
         return (
             <div>
                 <h2>STUFF</h2>
-                <p>Mauris sem velit, vehicula eget sodales vitae,
-                    rhoncus eget sapien:</p>
+                <p>{JSON.stringify(this.props)}</p>
                 <ol>
                     <li>Nulla pulvinar diam</li>
                     <li>Facilisis bibendum</li>
@@ -14,9 +33,11 @@ class Stuff extends React.Component {
                     <li>Eget erat</li>
                     <li>Id porttitor</li>
                 </ol>
+                <div onClick={this.goTo}>1111111</div>
             </div>
         );
     }
+
 }
 
 export default Stuff
